668855cad90158071549cd1b8821bdd6b7110647
BIPLOB <biplob2358@gmail.com>
2022-03-11 15:41:52 +0600

    add

---
+++ oprtation.js
@@ -0,0 +1,16 @@
+var price1 = 10;
+var price2 = 50;
+var sum = price1 + price2;
+console.log(sum);
+
+var minus = price2 - price1;
+console.log(minus);
+
+var multi = price1 * price2;
+console.log(multi);
+
+var div = price2 / price1;
+console.log(div);
+
+var mod = price2 % price1;
+console.log(mod);
