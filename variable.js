8bf16c278759a488d444eb8fb270d7108fa59c15
BIPLOB <biplob2358@gmail.com>
2022-03-11 13:03:00 +0600

    add

---
+++ variable.js
@@ -0,0 +1,13 @@
+var age = 22;
+console.log(age);
+console.log(typeof age);
+
+var name = "Biplob Ghosh";
+console.log(name);
+console.log(typeof name);
+
+var isHot = true;
+var isRich = false;
+console.log(isHot);
+console.log(isRich);
+console.log(typeof isHot);
