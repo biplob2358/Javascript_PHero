ae2da772a64ee89097a5f63c5219f671a384baed
BIPLOB <biplob2358@gmail.com>
2022-03-11 18:21:17 +0600

    add

---
+++ math.js
@@ -0,0 +1,17 @@
+var num = -5;
+var absolute = Math.abs(num);
+console.log(absolute);
+
+var num1 = 5.4321;
+var round = Math.round(num1);
+console.log(round);
+
+var result = Math.ceil(num1);
+console.log(result);
+
+var result = Math.floor(num1);
+console.log(result);
+
+var number = Math.random() * 100;
+var result = Math.round(number);
+console.log(result);
