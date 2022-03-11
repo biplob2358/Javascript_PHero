3285617f6fed92ccca23ff33825bd2ab57ad2d2e
BIPLOB <biplob2358@gmail.com>
2022-03-11 19:19:46 +0600

    add

---
+++ conditional.js
@@ -0,0 +1,16 @@
+var x = 12;
+if (x > 9) {
+  console.log("You pass");
+} else {
+  console.log("You fail");
+}
+
+var hall = true;
+var taka = 100;
+if (hall == true && taka >= 1000) {
+  console.log("You will allow");
+} else if (hall == true) {
+  console.log("You are waiting");
+} else {
+  console.log("You will not allow");
+}
