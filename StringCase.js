993d7c161c1deae5f065ec5baa8edf5321ff875e
BIPLOB <biplob2358@gmail.com>
2022-03-11 15:32:58 +0600

    add

---
+++ StringCase.js
@@ -0,0 +1,5 @@
+var promise = "I promise that I will work hard";
+console.log(promise.toLowerCase());
+console.log(promise.toUpperCase());
+console.log(promise.indexOf("will"));
+console.log(promise.split(" "));
---
+++ number.js
@@ -0,0 +1,10 @@
+var number1 = 25;
+var number2 = "15.5";
+number2 = parseFloat(number2);
+console.log(number1 + number2);
+
+var num1 = 0.1;
+var num2 = 0.3;
+var total = num1 + num2;
+total = total.toFixed(3);
+console.log(total);
