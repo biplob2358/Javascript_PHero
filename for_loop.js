377937e128c9729b04926057c3dbfa1c1769ef31
BIPLOB <biplob2358@gmail.com>
2022-03-12 12:38:18 +0600

    add

---
+++ for_loop.js
@@ -0,0 +1,9 @@
+for (i = 1; i <= 10; i++) {
+  console.log(i + " Biplob");
+}
+
+var nums = [44, 55, 66, 77, 88, 99, 11];
+for (var i = 0; i < nums.length; i++) {
+  var element = nums[i];
+  console.log(element);
+}
