fccf0bc58121861d6d09274e8703f7ee4e125ed1
BIPLOB <biplob2358@gmail.com>
2022-03-12 11:20:00 +0600

    add

---
+++ array_index.js
@@ -0,0 +1,13 @@
+var age = [11, 12, 13, 14, 15, 16, 17, 18];
+
+console.log(age);
+console.log(age[3]);
+
+var fAge = age[5];
+console.log(fAge);
+
+age[1] = 20;
+console.log(age[1]);
+
+console.log(age.indexOf(18));
+console.log(age.indexOf(30));
