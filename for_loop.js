a9bcce97a5c7d5684cbec28c776e0774ac183b37
BIPLOB <biplob2358@gmail.com>
2022-03-12 12:43:41 +0600

    add

--- for_loop.js
+++ for_loop.js
@@ -4,6 +4,6 @@ for (i = 1; i <= 10; i++) {
 
 var nums = [44, 55, 66, 77, 88, 99, 11];
 for (var i = 0; i < nums.length; i++) {
-  var element = nums[i];
-  console.log(element);
+  //   var element = nums[i];
+  console.log(nums[i]);
 }
