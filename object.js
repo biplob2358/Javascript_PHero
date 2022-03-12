551499b13037f4c7c81c6b9ef762a47f6a0a1d81
BIPLOB <biplob2358@gmail.com>
2022-03-12 16:57:10 +0600

    add

---
+++ object.js
@@ -0,0 +1,13 @@
+var student1 = { id: 121, phone: 10223213, stName: "Rahim" };
+var student2 = { id: 122, phone: 60223213, stName: "Karim" };
+
+console.log(student1);
+console.log(student2);
+
+console.log(student1.phone);
+console.log(student2.stName);
+
+student1.phone = 7564645;
+console.log(student1);
+student1.dept = "CSE";
+console.log(student1);
