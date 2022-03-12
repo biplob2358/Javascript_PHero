53cb9e0c33f75b2b4fce9088c6bcef9807a0049e
BIPLOB <biplob2358@gmail.com>
2022-03-12 13:16:28 +0600

    add

---
+++ switch_case.js
@@ -0,0 +1,23 @@
+cgp = 3.5;
+switch (cgp) {
+  case 5:
+    console.log("A+");
+    break;
+  case 4:
+    console.log("A");
+    break;
+  case 3.5:
+    console.log("A-");
+    break;
+  case 3:
+    console.log("B");
+    break;
+  case 2:
+    console.log("C");
+    break;
+  case 1:
+    console.log("E");
+    break;
+  default:
+    console.log("Please enter valid number");
+}
